2c12196067370836a858c07049c5ff56818b9d94
Michael <m.zamchalin@yandex.ru>
2022-11-22 13:19:57 +0300

    added example_001

---
+++ semenar_1/seminar_one/example_001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое  число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a / b == b)
+    Console.WriteLine("Yes");
+else 
+    Console.WriteLine("No");
